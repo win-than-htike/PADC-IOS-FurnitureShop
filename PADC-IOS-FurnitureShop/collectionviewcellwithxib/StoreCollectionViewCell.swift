36f077d180eaa29ef689264215d001310b41c329
Wai Yan Phyoe <waiyanphyoecu.wyp@gmail.com>
2018-12-15 15:30:36 +0630

    Added Collection View Cell

---
+++ PADC-IOS-FurnitureShop/collectionviewcellwithxib/StoreCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  StoreCollectionViewCell.swift
+//  PADC-IOS-FurnitureShop
+//
+//  Created by Elissa Software on 12/15/18.
+//  Copyright © 2018 padcmyanmar. All rights reserved.
+//
+
+import UIKit
+
+class StoreCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
